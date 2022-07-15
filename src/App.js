6338d30a1cea9d05b14103b0c7c3016e45bfa2aa
mnengwa <howardmnengwa@gmail.com>
2022-07-15 11:19:50 +0300

    feat: project structure 1. Create and implement an <Artist/> component 2. Cover concepts on looping through arrays, images in Create React App,    Props & state

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
 import './App.css';
+import Artist from './Artist';
+
+const artists = [
+    { _id: 234567890, name: 'Scar', image: 'Scar-Mkadinali-RbeVVolm.jpg', age: 27, bestLine: 'matwa twa msupa wako akipewa kiboko...'},
+    { _id: 987654689, name: 'Sewer sydr',  image: 'Scar-Mkadinali-RbeVVolm.jpg', age: 28, bestLine: 'Tunaguza bila ruhusa...'},
+    { _id: 123456789, name: 'Mad Munga',  image: 'Scar-Mkadinali-RbeVVolm.jpg', age: 30, bestLine: 'Mad Munga mjinga niko zone...'},
+    { _id: 456789654, name: 'Dyan Code',  image: 'Scar-Mkadinali-RbeVVolm.jpg', age: 29, bestLine: '...Napenda a guy like Kanyari...he getting that paper..'},
+];
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Wakadinali</h1>
       </header>
+
+      <main>
+        {
+            artists.map(({_id, name, age, image, bestLine}) => {
+                return <Artist key={_id} image={image} name={name} age={age} bestLine={bestLine} />
+            })
+        }
+      </main>
     </div>
   );
 }
