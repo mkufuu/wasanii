2d9d9cd7bc370ca085fa4f02162336a9026aedab
Vickopiyo <victor.okoth@student.moringaschool.com>
2022-09-01 11:57:42 +0300

    DONE

--- src/components/Colony.js
+++ src/components/Colony.js
@@ -1,7 +1,9 @@
 import Artist from "./Artist";
+import SearchBar from "./SearchBar";
 
 const Colony = ({name, artists}) => {
     return <section className="colony py-3">
+        <SearchBar />
         <header className="mb-5 text-center">
             <h2>{name}</h2>
             <p class="text-muted">Music Group</p>
